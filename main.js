db0d81587c7bfbc187f8868bc8f41d62520d97b2
Adam McLean <amclean@zoemg.com>
2015-09-01 07:48:02 -0700

    Adds alerts in JS

--- main.js
+++ main.js
@@ -9,11 +9,16 @@ function select_sprite(){
 
     if (num <= .33){
       $("#imagesprite").css("background-position", $("#imagesprite").css("background-position"));
+      alert($("#imagesprite").css("background-position"));
+
     }
     else if (num > .33 && num < .66){
       $("#imagesprite").css("background-position", $("#imagespriteA").css("background-position"));
+      alert($("#imagespriteA").css("background-position"));
     }
     else {
       $("#imagesprite").css("background-position", $("#imagespriteB").css("background-position"));
+      alert($("#imagespriteB").css("background-position"));
+
     }
 };
